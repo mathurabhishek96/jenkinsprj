9d7b08e3dd79fa18ec5711c65754b55ec34299ea
mathurabhishek73 <mathurabhishek73@gmail.com>
2025-03-22 08:23:33 +0000

    dawd

--- src/jenkins/jenkins.java
+++ src/jenkins/jenkins.java
@@ -2,6 +2,6 @@ package jenkins;
 
 public class jenkins {
     public static void main(String[] args) {
-        System.out.println("Hjelol, Jen]s! The uild is successful.");
+        System.out.println("Helllelol, Jen]s! The uild is successful.");
     }
 }
