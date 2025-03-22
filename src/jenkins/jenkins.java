0dbdc14d3735ffb70ae142bcb713d5bedc18061f
mathurabhishek73 <mathurabhishek73@gmail.com>
2025-03-22 06:41:58 +0000

    m

--- src/jenkins/jenkins.java
+++ src/jenkins/jenkins.java
@@ -2,6 +2,6 @@ package jenkins;
 
 public class jenkins {
     public static void main(String[] args) {
-        System.out.println("Helol, Jenkins! The build is successful.");
+        System.out.println("Helol, Jenkins! The uild is successful.");
     }
 }
